c43a8c83b76dd07d9fa28894e0adf0250992be7c
Denis-22y <114328975+Denis-22y@users.noreply.github.com>
2024-06-29 17:41:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { vexo } from 'vexo-analytics';
 
 if(!__DEV__)
-    vexo('38e852e1-2d9d-4809-b476-4ed150b87153');
+    vexo('API KEY');
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
